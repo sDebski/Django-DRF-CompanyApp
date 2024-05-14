38b667a7cafee04db87d2811914ab82eae32503d
Szymon Dębski <skwdebski@gmail.com>
2024-05-14 15:52:18 +0200

    added history_log, action draft

--- company/models.py
+++ company/models.py
@@ -1,5 +1,8 @@
 # Create your models here.
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Worker(models.Model):
@@ -61,3 +64,47 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.content[:50]  # Zwraca pierwsze 50 znaków komentarza
+
+
+# class Action(models.Model):
+#     class ActionTypeChoices(models.TextChoices):
+#         task_added = "dodanie_zadania", "Dodanie zadania"
+#         task_closed = "zamkniecie_zadania", "Zamknięcie zadania"
+#         status = "edycja_statusu", "Edycja statusu"
+#         title = "edycja_tytulu", "Edycja tytułu"
+#         description = "edycja_opisu", "Edycja opisu"
+#         label = "edycja_etykiety", "Edycja etykiet"
+#         assigned_to = "edycja_adresata", "Edycja adresata"
+#         project_name = "edycja_nazwy_projektu", "Edycja nazwy projektu"
+
+#     type = models.CharField(max_length=256) #, choices=ActionTypeChoices)
+
+#     # details = {
+#     #     "label_1": "stary tytuł",
+#     #     "value_1":  "old_title",
+#     #     "label_2": "nowy tytuł", # optional
+#     #     "value_2": "new_title", # optional
+#     #   }
+#     details = models.JSONField(null=True, blank=True)
+
+#     def __str__(self) -> str:
+#         return self.type
+
+
+class HistoryLog(models.Model):
+    task = models.ForeignKey(
+        Task, on_delete=models.CASCADE, related_name="history_logs"
+    )
+    # actions = models.ManyToManyField(
+    #     Action, related_name="history_logs"
+    # )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="history_logs"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.user} - {self.created_at.date()}"
